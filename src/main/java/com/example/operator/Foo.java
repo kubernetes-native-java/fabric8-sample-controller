927c7e3a1a6d8c88c016993891684bdaaa41868d
Josh Long <josh@joshlong.com>
2021-12-29 22:09:05 -0800

    operations

--- src/main/java/com/example/operator/Foo.java
+++ src/main/java/com/example/operator/Foo.java
@@ -1,14 +1,25 @@
 package com.example.operator;
 
 import io.fabric8.kubernetes.api.model.Namespaced;
+import io.fabric8.kubernetes.api.model.ObjectMeta;
 import io.fabric8.kubernetes.client.CustomResource;
 import io.fabric8.kubernetes.model.annotation.Group;
 import io.fabric8.kubernetes.model.annotation.Plural;
 import io.fabric8.kubernetes.model.annotation.Version;
 
 @Version("v1alpha1")
-@Group("samplecontroller.k8s.io")
+@Group("foocontroller.k8s.io")
 @Plural("foos")
 public class Foo extends CustomResource<FooSpec, FooStatus> implements Namespaced {
+
+	public Foo(FooSpec spec, ObjectMeta objectMeta, FooStatus status) {
+		var cloneFoo = this;
+		cloneFoo.setSpec(spec);
+		cloneFoo.setMetadata(objectMeta);
+		cloneFoo.setStatus(status);
+	}
+
+	public Foo() {
+	}
 
 }
--- src/main/java/com/example/operator/OperatorApplication.java
+++ src/main/java/com/example/operator/OperatorApplication.java
@@ -1,11 +1,23 @@
 package com.example.operator;
 
+import io.fabric8.kubernetes.api.model.apps.Deployment;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.dsl.MixedOperation;
+import io.fabric8.kubernetes.client.dsl.Resource;
+import io.fabric8.kubernetes.client.dsl.base.CustomResourceDefinitionContext;
+import io.fabric8.kubernetes.client.informers.SharedIndexInformer;
+import io.fabric8.kubernetes.client.informers.SharedInformerFactory;
+import io.fabric8.kubernetes.model.Scope;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationEvent;
 import org.springframework.context.annotation.Bean;
-import org.springframework.nativex.hint.TypeHint;
 
-import static org.springframework.nativex.hint.TypeAccess.*;
+import java.util.Locale;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 /**
  * This example was lifted
@@ -15,16 +27,63 @@ import static org.springframework.nativex.hint.TypeAccess.*;
  * @author Rohan Kumar
  * @author Josh Long
  */
+@Slf4j
 @SpringBootApplication
 public class OperatorApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(OperatorApplication.class, args);
 	}
 
 	@Bean
-	Runner runner() {
-		return new Runner();
+	ScheduledExecutorService scheduledExecutorService() {
+		return Executors.newScheduledThreadPool(Runtime.getRuntime().availableProcessors());
+	}
+
+	@Bean
+	SharedInformerFactory sharedInformerFactory(KubernetesClient client) {
+		return client.informers();
+	}
+
+	@Bean
+	MixedOperation<Foo, FooList, Resource<Foo>> fooClient(KubernetesClient client) {
+		return client.resources(Foo.class, FooList.class);
+	}
+
+	@Bean
+	SharedIndexInformer<Deployment> deploymentSharedIndexInformer(SharedInformerFactory sharedInformerFactory) {
+		return sharedInformerFactory.sharedIndexInformerFor(Deployment.class, 1000);
+	}
+
+	@Bean
+	SharedIndexInformer<Foo> fooSharedIndexInformer(SharedInformerFactory sharedInformerFactory) {
+		return sharedInformerFactory.sharedIndexInformerFor(Foo.class, 1000);
+	}
+
+	@Bean
+	CustomResourceDefinitionContext foosCustomResourceDefinitionContext() {
+		var value = Scope.NAMESPACED.value();
+		var plural = Foo.class.getSimpleName().toLowerCase(Locale.ROOT) + "s";
+		return new CustomResourceDefinitionContext.Builder() //
+				.withVersion("v1alpha1") //
+				.withScope(value)//
+				.withGroup("foocontroller.k8s.io") //
+				.withPlural(plural)//
+				.build();
+	}
+
+	@Bean(destroyMethod = "close")
+	KubernetesClient kubernetesClient() {
+		return new DefaultKubernetesClient();
+	}
+
+	@Bean
+	FooController fooController(SharedInformerFactory sharedInformerFactory, KubernetesClient client,
+			MixedOperation<Foo, FooList, Resource<Foo>> fooClient,
+			SharedIndexInformer<Deployment> deploymentSharedIndexInformer,
+			SharedIndexInformer<Foo> fooSharedIndexInformer) {
+		return new FooController(client, fooClient, deploymentSharedIndexInformer, fooSharedIndexInformer,
+				client.getNamespace(), sharedInformerFactory);
 	}
 
 }
